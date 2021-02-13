2b1511019d39bcf75bda35cf587814f967e365cf
yangon99 <yangon99_all@outlook.com>
2021-02-13 20:05:56 +0800

    add pkg: cgproxy-gapoon-git

---
+++ pkgbuilds/cgproxy-gapoon-git/lilac.py
@@ -0,0 +1,13 @@
+from lilaclib import edit_file
+
+def pre_build():
+    for line in edit_file('PKGBUILD'):
+        if line.startwith('pkgname='):
+            print('pkgname=cgproxy-gapoon-git')
+        print(line)
+        print('''
+        prepare(){
+            cd ${srcdir}/${pkgname}
+            git fetch origin pull/29/head:huyz-pr
+            git merge --no-commit huyz-pr
+        }''')
