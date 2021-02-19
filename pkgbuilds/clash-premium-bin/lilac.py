7d180242ab988664a263fae77d72b7ff43143a4f
yangon99 <yangon99_all@outlook.com>
2021-02-20 00:23:32 +0800

    update lilac file for clash-premium-bin

---
+++ pkgbuilds/clash-premium-bin/lilac.py
@@ -0,0 +1,9 @@
+from lilaclib import *
+
+def pre_build():
+    if _G.newver is not None:
+        update_pkgver_and_pkgrel(_G.newver)
+    else:
+        raise Exception("new version check failed (new ver is None)")
+
+# def post_build():
